3ee85c7a932d6a36a4090a7941b7eeef783d129a
Maximiliano Bisurgi <mbisurgi@MacBook-Pro-de-Maximiliano.local>
2020-09-19 01:49:04 -0300

    Testing db

--- dist/config/database.js
+++ dist/config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-	db_test: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM_DEV',
-	db_local: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM_DEV',
-	db_dev: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM_DEV'
+	db_test: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM',
+	db_local: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM',
+	db_dev: 'mssql://sa:maximati@bybgas.dyndns.org:1730/CRM'
 };
 
 // module.exports = {
