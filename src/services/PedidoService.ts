8b07086c56af279d953fa05b33000feac43657c1
mbisurgisoshace <maximiliano.bisurgi@gmail.com>
2023-06-01 14:25:06 -0300

    Use rambda

--- src/services/PedidoService.ts
+++ src/services/PedidoService.ts
@@ -1,14 +1,14 @@
-import { omit } from 'lodash'
 import * as moment from 'moment';
 import knex from '../db/connection';
 
 import { formatKeys } from '../utils/utils';
+import { omit } from 'ramda';
 export default class PedidoService {
   public static insertarPedido = async (pedido) => {
     let pedidoId = null;
-    const pedidoEnc = omit(pedido, 'items');
+    const pedidoEnc = omit(['items'], pedido);
     pedidoEnc.fecha = moment(pedidoEnc.fecha, 'DD-MM-YYYY').format('YYYY-MM-DD')
-    const pedidoDet = pedido.items.map(item => formatKeys(omit(item, 'precio')));
+    const pedidoDet = pedido.items.map(item => formatKeys(omit(['precio'], item)));
     console.log('enc', pedidoEnc);
     console.log('det', pedidoDet);
     await knex.transaction(async (trx) => {
